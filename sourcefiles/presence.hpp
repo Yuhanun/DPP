a8d37c5008b0c75e33ab373901aa048e0092e756
LucasRaymann <50414765+LucasRaymann@users.noreply.github.com>
2019-06-05 19:23:58 +0200

    fixed a little thingy in discord::Presence

--- sourcefiles/presence.hpp
+++ sourcefiles/presence.hpp
@@ -6,10 +6,15 @@
 discord::Presence::Presence(const nlohmann::json& j)
 		: user(new discord::User{ discord::get_value(j, "user", nlohmann::json({})) }),
 		  guild_id(new discord::Guild{ discord::detail::bot_instance->get_guild(to_sf(discord::get_value(j, "guild_id", "0"))) }),
-		  status(discord::get_value(j, "status", "")),
-		  client_status{ discord::get_value(j["client_status"], "desktop", ""),
-		                 discord::get_value(j["client_status"], "mobile", ""),
-		                 discord::get_value(j["client_status"], "web", "") } {
+		  status(discord::get_value(j, "status", "")) {
+	if (j.contains("client_status")) {
+		auto client_status_obj = get_value(j, "client", nlohmann::json({}));
+		client_status = {
+			get_value(client_status_obj, "desktop", ""),
+			get_value(client_status_obj, "mobile", ""),
+			get_value(client_status_obj, "web", "")
+		};
+	}
 	if (j.contains("roles"))
 		for (const auto& each : j["roles"])
 			roles.emplace_back(new discord::Role{ each });
